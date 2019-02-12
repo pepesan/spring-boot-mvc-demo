faea74dbaacc8d69791984dc812f56a2e376331b
pepesan <pepesan@gmailcom>
2019-02-12 12:11:30 +0100

    mejorada la clase de entidad con la biblioteca lombok

--- src/main/java/com/cursosdedesarrollo/springbootdemo/Dato.java
+++ src/main/java/com/cursosdedesarrollo/springbootdemo/Dato.java
@@ -1,30 +1,13 @@
 package com.cursosdedesarrollo.springbootdemo;
 
+import lombok.Data;
+
 import java.io.Serializable;
+
+@Data
 
 public class Dato implements Serializable {
 
     private String cadena;
-    public Dato() {
-        this.cadena = "";
-    }
 
-    public Dato(String cadena) {
-        this.cadena = cadena;
-    }
-
-    public String getCadena() {
-        return cadena;
-    }
-
-    public void setCadena(String cadena) {
-        this.cadena = cadena;
-    }
-
-    @Override
-    public String toString() {
-        return "Dato{" +
-                "cadena='" + cadena + '\'' +
-                '}';
-    }
 }
